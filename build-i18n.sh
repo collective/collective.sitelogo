63b4d192f120a7eeeed346c72ca31acf3bfda610
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2015-10-08 13:24:50 -0430

    Updated the build-i18n.sh file wiht plone domain

--- build-i18n.sh
+++ build-i18n.sh
@@ -4,3 +4,5 @@ I18NPATH=src/collective/sitelogo
 DOMAIN=collective.sitelogo
 $I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --create $DOMAIN $I18NPATH
 $I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po
+$I18NDUDE rebuild-pot --pot $I18NPATH/locales/plone.pot --create plone $I18NPATH/profiles
+$I18NDUDE sync --pot $I18NPATH/locales/plone.pot $I18NPATH/locales/*/LC_MESSAGES/plone.po
